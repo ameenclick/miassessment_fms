44b103dcec0e7b44027425eec45c2b33726bc72d
ameeninfobhan <developer@infobhan.cloud>
2023-06-13 06:44:29 +0530

    Assessment Report button change

--- components/user.js
+++ components/user.js
@@ -193,6 +193,7 @@ export default function User(props){
                             <th scope="col">Code</th>
                             <th scope="col">User Name</th>
                             <th scope="col">Franchise</th>
+                            <th>Report</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -210,11 +211,14 @@ export default function User(props){
                         <td data-bs-toggle="modal" data-bs-target="#userDetails">{user.name}</td>
                         <td>{user.franchiseCode? user.franchiseCode: "MI_APP"}</td>
                         <td>
-                            <a href={user.report_url} target="_blank" className='btn btn-primary'>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file-earmark-bar-graph" viewBox="0 0 16 16">
-                                    <path d="M10 13.5a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-6a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v6zm-2.5.5a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-1zm-3 0a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-1z"/>
-                                    <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
-                                </svg> Report
+                            <a href={user.report_url} target="_blank" className='btn btn-outline-primary rounded-pill'>
+                                Open
+                            </a> &nbsp;
+                            <a href={`${process.env.host}/report/${user.userCode}`} target="_blank" className='btn btn-outline-primary'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                                    <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                                    <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                                </svg>
                             </a>    
                         </td>
                         </tr>
